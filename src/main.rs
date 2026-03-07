18afd81c7926f2f1e5637ea25cf67a7c6e3db322
Satyam Lal <ssatyamlal@gmail.com>
2026-03-08 02:55:26 +0530

    feat: refactor sorter to use match patterns for multi-extension support

--- src/main.rs
+++ src/main.rs
@@ -33,5 +33,28 @@ fn main() {
 
         let path = entry.path();
         println!("Found!: {}", path.display());
+
+        let extension = path.extension().and_then(|s| s.to_str());
+
+        let target_folder = match extension {
+            Some("rs") => "Rust code",
+            Some("pdf") | Some("doc") | Some("docx") => "documents",
+            Some("jpg") | Some("png") | Some("gif") => "images",
+            _ => continue,
+        };
+
+        let mut dest_dir = PathBuf::from(directory_path);
+        dest_dir.push(target_folder);
+
+        fs::create_dir_all(&dest_dir).ok();
+
+        if let Some(file_name) = path.file_name() {
+            let dest_path = dest_dir.join(file_name);
+
+            println!("Moving {:?} to {}", path.file_name().unwrap(), target_folder);
+            fs::rename(&path, dest_path).unwrap_or_else(|err| {
+                eprintln!("Move failed: {}", err);
+            });
+        }
     }
 }
